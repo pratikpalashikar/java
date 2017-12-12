f7a68ba867908ac5d8cd08c2fb8f606696bb6f7b
Pratik <pratik@Pratiks-MacBook-Pro.local>
2017-12-11 23:02:59 -0500

    made the class as the package private and the removed the public access modifier

--- exercises/matrix/.meta/src/reference/java/Matrix.java
+++ exercises/matrix/.meta/src/reference/java/Matrix.java
@@ -5,7 +5,7 @@ class Matrix {
     private static Pattern spacePattern = Pattern.compile(" ");
     private static Pattern newlinePattern = Pattern.compile("\\n");
 
-     Matrix(String matrixAsString) {
+     public Matrix(String matrixAsString) {
         String[] rows = newlinePattern.split(matrixAsString);
         matrix = new int[rows.length][];
         for (int i = 0; i < rows.length; i++) {
