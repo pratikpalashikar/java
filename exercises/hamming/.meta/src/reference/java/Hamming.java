b976eef58ed88bbae95c54088d80ab7ca4dabea5
Pratik <pratik@Pratiks-MacBook-Pro.local>
2017-12-12 19:22:59 -0500

    removed the access modifier of the class Hamming and also the methods involving it

--- exercises/hamming/.meta/src/reference/java/Hamming.java
+++ exercises/hamming/.meta/src/reference/java/Hamming.java
@@ -9,7 +9,7 @@ class Hamming {
         return hammingDistance;
     }
 
-    private int computeHammingDistance(String leftStrand, String rightStrand) {
+    int computeHammingDistance(String leftStrand, String rightStrand) {
         if(leftStrand.length() != rightStrand.length()) {
             throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
         }
@@ -22,7 +22,7 @@ class Hamming {
         return distance;
     }
 
-    private int hammingContributionAt(int index, String leftStrand, String rightStrand) {
+    int hammingContributionAt(int index, String leftStrand, String rightStrand) {
       return leftStrand.charAt(index) != rightStrand.charAt(index) ? 1 : 0;
     }
 
